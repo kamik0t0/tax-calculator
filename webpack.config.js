1a76a46d9b32b40440eeb432215f94345db9e918
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-10-06 17:51:37 +0500

    info main page

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     output: {
         filename: filename("js"),
         path: path.resolve(__dirname, "dist"),
-        // publicPath: "/tax-calculator/",
+        publicPath: "/tax-calculator/",
     },
     plugins: [
         new HtmlWebpackPlugin({
